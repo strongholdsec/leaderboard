f343e4572297a2cd223c1010865d23e14051e269
asimaranov <a.simaranov@gmail.com>
2023-09-25 02:16:47 +0800

    fix: lint

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,11 @@ const RootLayout = function RootLayout({
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="manifest" href="manifest.json" />
-        <link rel="shortcut icon" href="/icons/favicon.ico" type="image/x-icon" />
+        <link
+          rel="shortcut icon"
+          href="/icons/favicon.ico"
+          type="image/x-icon"
+        />
         <title>Auditor board</title>
         <Fonts />
       </head>
