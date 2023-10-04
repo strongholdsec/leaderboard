de4f752a12cb9872d79d55271b2d1b185e7f6bee
Puzanira <puzanova.ira2011@yandex.ru>
2023-10-04 22:38:46 +0300

    feat: added csp policies and cors headers

--- next.config.js
+++ next.config.js
@@ -3,7 +3,40 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
-  output: 'standalone'
-}
+  output: 'standalone',
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on',
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'same-origin',
+          },
+          {
+            key: 'x-content-type-options',
+            value: 'nosniff',
+          },
+          { key: 'x-xss-protection', value: '1' },
+          { key: 'x-download-options', value: 'noopen' },
+        ],
+      },
+      {
+        // required for gnosis save apps
+        source: '/manifest.json',
+        headers: [{ key: 'Access-Control-Allow-Origin', value: '*' }],
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
