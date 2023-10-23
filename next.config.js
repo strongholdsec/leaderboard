d2e8d76267d7fa65f0ba3cd783577fd12b5777ef
Puzanira <puzanova.ira2011@yandex.ru>
2023-10-23 13:00:45 +0300

    feat: added basePath to next-config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
+
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
+
 const nextConfig = {
+  basePath,
   compiler: {
     styledComponents: true,
   },
