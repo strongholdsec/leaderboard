ddc6d350c6016ee317eba0964af52530cdb611d3
asimaranov <a.simaranov@gmail.com>
2023-10-27 18:57:40 +0300

    fix: Added new token data format support

--- utils/parseTokenParams.ts
+++ utils/parseTokenParams.ts
@@ -6,6 +6,21 @@ export const parseTokenParams = (params: readonly Hex[]): ITokenParams => {
   const data = params.map((token) => hexToString(token, { size: 32 }));
 
   switch (data.length) {
+    // CTF, Audit, critical, high, medium, low, unique critical, unique high, unique medium, unique low
+    case 9:
+      return {
+        CTF: 0,
+        audit: 0,
+        critical: parseInt(data[0]),
+        high: parseInt(data[1]),
+        medium: parseInt(data[2]),
+        low: parseInt(data[3]),
+        rewards: parseInt(data[4]),
+        uniqueCritical: parseInt(data[5]),
+        uniqueHigh: parseInt(data[6]),
+        uniqueMedium: parseInt(data[7]),
+        uniqueLow: parseInt(data[8]),
+      };
     // CTF, Audit, critical, high, medium, low
     case 6:
       return {
@@ -16,6 +31,10 @@ export const parseTokenParams = (params: readonly Hex[]): ITokenParams => {
         medium: parseInt(data[4]),
         low: parseInt(data[5]),
         rewards: 0,
+        uniqueCritical: 0,
+        uniqueHigh: 0,
+        uniqueMedium: 0,
+        uniqueLow: 0,
       };
     // critical, high, medium, low, rewards
     case 5:
@@ -27,6 +46,10 @@ export const parseTokenParams = (params: readonly Hex[]): ITokenParams => {
         medium: parseInt(data[2]),
         low: parseInt(data[3]),
         rewards: parseInt(data[4]),
+        uniqueCritical: 0,
+        uniqueHigh: 0,
+        uniqueMedium: 0,
+        uniqueLow: 0,
       };
     default:
       return {
@@ -37,6 +60,10 @@ export const parseTokenParams = (params: readonly Hex[]): ITokenParams => {
         medium: 0,
         low: 0,
         rewards: 0,
+        uniqueCritical: 0,
+        uniqueHigh: 0,
+        uniqueMedium: 0,
+        uniqueLow: 0,
       };
   }
 };
