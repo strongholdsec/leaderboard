5f0f5d50e75c9bdd0e9e873f87e10cf8b21d9cc2
Puzanira <puzanova.ira2011@yandex.ru>
2023-11-06 11:36:17 +0300

    feat: added border to account, fixed farm dates

--- features/common/layout/layout/index.tsx
+++ features/common/layout/layout/index.tsx
@@ -3,7 +3,7 @@ import { FC, PropsWithChildren } from 'react';
 
 export const Layout: FC<PropsWithChildren> = ({ children }) => (
   <Container
-    maxWidth="lg"
+    maxWidth="xl"
     component="main"
     sx={{ pt: { lg: 22, xs: 15 }, pb: 6 }}
   >
