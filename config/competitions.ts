a23a840747fb9e7cf57b0b8ad2d1b57cb64da317
Puzanira <puzanova.ira2011@yandex.ru>
2024-01-11 21:20:44 +0300

    feat: added icons for Shiftam, UNCX and Netherfi

--- config/competitions.ts
+++ config/competitions.ts
@@ -5,6 +5,9 @@ import FarmSpring from 'assets/icons/farm_spring.svg';
 import FarmWinter from 'assets/icons/farm_winter.svg';
 import LidoIcon from 'assets/icons/lido.svg';
 import MoonwellIcon from 'assets/icons/moonwel.svg';
+import NetherFi from 'assets/icons/netherfi.jpg';
+import Shiftam from 'assets/icons/shiftam.png';
+import UNCXIcon from 'assets/icons/uncx.svg';
 
 export type CompetitionType = 'farm' | 'contest';
 export type SeasonType = 2022 | 2023;
@@ -111,7 +114,7 @@ export const competitionInfo: CompetitionInfo[] = [
     startDate: new Date('2023/08/03'),
     endDate: new Date('2023/11/17'),
     season: 2023,
-    imageSrc: '',
+    imageSrc: NetherFi.src,
   },
   {
     id: 9,
@@ -120,7 +123,7 @@ export const competitionInfo: CompetitionInfo[] = [
     startDate: new Date('2023/11/23'),
     endDate: new Date('2023/12/11'),
     season: 2023,
-    imageSrc: '',
+    imageSrc: UNCXIcon,
   },
   {
     id: 10,
@@ -129,7 +132,7 @@ export const competitionInfo: CompetitionInfo[] = [
     startDate: new Date('2023/12/08'),
     endDate: new Date('2023/12/25'),
     season: 2023,
-    imageSrc: '',
+    imageSrc: Shiftam.src,
   },
   {
     id: 11,
